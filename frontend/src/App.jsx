770ca6f424b91e9f5a3fb186bb6e286edac79691
asifmohamed491-code <asifmohamed491@gmail.com>
2026-03-11 21:50:46 +0530

    updated some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   }
   function send() {
     setstatus(true)
-    axios.post("https://bulk-mail-sender-4xye.onrender.com/sendmail", { msg: msg , emailList:emailList})
+    axios.post("https://bulk-mail-sender-1-u9hg.onrender.com/sendmail", { msg: msg , emailList:emailList})
       .then(function (data) {
         if (data.data === true) {
           alert("Email Sent Successfully")
